3604be4352db0eb29b56fa16bed51c827759b5eb
ridhokpp <ohdirpratama@gmail.com>
2018-11-08 20:47:12 +0700

    dbhhost

--- berguru_data.sql
+++ berguru_data.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-11-08 20:43:40
+Date: 2018-11-08 20:43:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,6 +27,38 @@ CREATE TABLE `attachment` (
   KEY `id_materi_attachment` (`id_materi`),
   CONSTRAINT `id_materi_attachment` FOREIGN KEY (`id_materi`) REFERENCES `materi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of attachment
+-- ----------------------------
+INSERT INTO `attachment` VALUES ('1', '2', 'materi/Kimia/Aplikasi_Zakat.pdf');
+INSERT INTO `attachment` VALUES ('2', '2', 'materi/Kimia/atlassian-git-cheatsheet.pdf');
+INSERT INTO `attachment` VALUES ('3', '2', 'materi/Kimia/buku-data-penduduk-sasaran-program-pembangunan-kesehatan-2011-2014.pdf');
+INSERT INTO `attachment` VALUES ('4', '1', 'materi/Kimia/Aplikasi_Zakat.pdf');
+INSERT INTO `attachment` VALUES ('5', '1', 'materi/Kimia/atlassian-git-cheatsheet.pdf');
+INSERT INTO `attachment` VALUES ('6', '1', 'materi/Kimia/buku-data-penduduk-sasaran-program-pembangunan-kesehatan-2011-2014.pdf');
+INSERT INTO `attachment` VALUES ('7', '1', 'materi/Kimia/downloadfile.pdf');
+INSERT INTO `attachment` VALUES ('8', '1', 'materi/Kimia/krs140535605307.pdf');
+INSERT INTO `attachment` VALUES ('9', '1', 'materi/Kimia/krs140535605550.pdf');
+INSERT INTO `attachment` VALUES ('10', '1', 'materi/Kimia/krs140535605590__20173.pdf');
+INSERT INTO `attachment` VALUES ('11', '1', 'materi/Kimia/krs140535605806__20173.pdf');
+INSERT INTO `attachment` VALUES ('12', '1', 'materi/Kimia/krs140535605814.pdf');
+INSERT INTO `attachment` VALUES ('13', '1', 'materi/Kimia/krs140535605990.pdf');
+INSERT INTO `attachment` VALUES ('14', '1', 'materi/Kimia/krs140535606662.pdf');
+INSERT INTO `attachment` VALUES ('15', '1', 'materi/Kimia/krs140535605307.pdf');
+INSERT INTO `attachment` VALUES ('16', '1', 'materi/Kimia/krs140535605550.pdf');
+INSERT INTO `attachment` VALUES ('17', '1', 'materi/Kimia/krs140535605590__20173.pdf');
+INSERT INTO `attachment` VALUES ('18', '1', 'materi/Kimia/krs140535605806__20173.pdf');
+INSERT INTO `attachment` VALUES ('19', '1', 'materi/Kimia/krs140535605814.pdf');
+INSERT INTO `attachment` VALUES ('20', '1', 'materi/Kimia/krs140535605990.pdf');
+INSERT INTO `attachment` VALUES ('21', '1', 'materi/Kimia/krs140535606662.pdf');
+INSERT INTO `attachment` VALUES ('22', '1', 'materi/Kimia/krs140535605307.pdf');
+INSERT INTO `attachment` VALUES ('23', '1', 'materi/Kimia/krs140535605550.pdf');
+INSERT INTO `attachment` VALUES ('24', '1', 'materi/Kimia/krs140535605590__20173.pdf');
+INSERT INTO `attachment` VALUES ('25', '1', 'materi/Kimia/krs140535605806__20173.pdf');
+INSERT INTO `attachment` VALUES ('26', '1', 'materi/Kimia/krs140535605814.pdf');
+INSERT INTO `attachment` VALUES ('27', '1', 'materi/Kimia/krs140535605990.pdf');
+INSERT INTO `attachment` VALUES ('28', '1', 'materi/Kimia/krs140535606662.pdf');
 
 -- ----------------------------
 -- Table structure for direct_message
@@ -47,6 +79,10 @@ CREATE TABLE `direct_message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of direct_message
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for kategori
 -- ----------------------------
 DROP TABLE IF EXISTS `kategori`;
@@ -61,6 +97,11 @@ CREATE TABLE `kategori` (
   `nama_folder` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of kategori
+-- ----------------------------
+INSERT INTO `kategori` VALUES ('1', 'Kimia', '2018-11-05', '1', '3', 'ACTIVE', '', 'materi/Kimia');
 
 -- ----------------------------
 -- Table structure for komentar
@@ -85,6 +126,13 @@ CREATE TABLE `komentar` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of komentar
+-- ----------------------------
+INSERT INTO `komentar` VALUES ('1', 'lalalala\r\n', '2018-11-06 07:19:27', '2', '1', '0', null, null, '3');
+INSERT INTO `komentar` VALUES ('2', 'lilili', '2018-11-06 07:20:15', '2', '1', '0', null, null, '3');
+INSERT INTO `komentar` VALUES ('3', 'lolololo', '2018-11-06 07:21:19', '2', '1', '0', null, null, '1');
+
+-- ----------------------------
 -- Table structure for komentar_message
 -- ----------------------------
 DROP TABLE IF EXISTS `komentar_message`;
@@ -103,6 +151,10 @@ CREATE TABLE `komentar_message` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of komentar_message
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for lowongan
 -- ----------------------------
 DROP TABLE IF EXISTS `lowongan`;
@@ -117,6 +169,10 @@ CREATE TABLE `lowongan` (
   `tanggal` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of lowongan
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for materi
@@ -141,6 +197,10 @@ CREATE TABLE `materi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of materi
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for max_notif_id_per_user
 -- ----------------------------
 DROP TABLE IF EXISTS `max_notif_id_per_user`;
@@ -150,6 +210,19 @@ CREATE TABLE `max_notif_id_per_user` (
   `max_notif_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of max_notif_id_per_user
+-- ----------------------------
+INSERT INTO `max_notif_id_per_user` VALUES ('1', '1', '1');
+INSERT INTO `max_notif_id_per_user` VALUES ('2', '2', '9');
+INSERT INTO `max_notif_id_per_user` VALUES ('3', '3', '6');
+INSERT INTO `max_notif_id_per_user` VALUES ('4', '4', '14');
+INSERT INTO `max_notif_id_per_user` VALUES ('5', '5', '0');
+INSERT INTO `max_notif_id_per_user` VALUES ('6', '6', '6');
+INSERT INTO `max_notif_id_per_user` VALUES ('7', '7', '0');
+INSERT INTO `max_notif_id_per_user` VALUES ('8', '2', '9');
+INSERT INTO `max_notif_id_per_user` VALUES ('9', '3', '6');
 
 -- ----------------------------
 -- Table structure for message
@@ -166,6 +239,10 @@ CREATE TABLE `message` (
   KEY `siapa_tendik_message` (`siapa`),
   CONSTRAINT `siapa_tendik_message` FOREIGN KEY (`siapa`) REFERENCES `pengguna` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of message
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for notif
@@ -186,6 +263,17 @@ CREATE TABLE `notif` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of notif
+-- ----------------------------
+INSERT INTO `notif` VALUES ('3', 'pertanyaan', '1', '3', 'mahasiswa', '2018-11-06 07:18:41', null, null);
+INSERT INTO `notif` VALUES ('4', 'komentar', '1', '2', '3', '2018-11-06 07:19:27', null, null);
+INSERT INTO `notif` VALUES ('5', 'komentar', '1', '2', '3', '2018-11-06 07:20:15', null, null);
+INSERT INTO `notif` VALUES ('6', 'komentar', '1', '2', '3', '2018-11-06 07:21:19', null, null);
+INSERT INTO `notif` VALUES ('7', 'ratingKomentar', '1', '3', '2', '2018-11-06 07:26:41', null, null);
+INSERT INTO `notif` VALUES ('8', 'ratingKomentar', '1', '3', '2', '2018-11-06 07:26:44', null, null);
+INSERT INTO `notif` VALUES ('9', 'ratingKomentar', '1', '3', '2', '2018-11-06 07:26:45', null, null);
+
+-- ----------------------------
 -- Table structure for notif_flag
 -- ----------------------------
 DROP TABLE IF EXISTS `notif_flag`;
@@ -199,6 +287,17 @@ CREATE TABLE `notif_flag` (
   KEY `id_notif_notif` (`id_notif`),
   CONSTRAINT `id_notif_notif` FOREIGN KEY (`id_notif`) REFERENCES `notif` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of notif_flag
+-- ----------------------------
+INSERT INTO `notif_flag` VALUES ('3', '2', '3', '1', '0');
+INSERT INTO `notif_flag` VALUES ('4', '3', '6', '1', '0');
+INSERT INTO `notif_flag` VALUES ('5', '3', '5', '1', '0');
+INSERT INTO `notif_flag` VALUES ('6', '3', '4', '1', '0');
+INSERT INTO `notif_flag` VALUES ('7', '2', '9', '1', '0');
+INSERT INTO `notif_flag` VALUES ('8', '2', '8', '1', '0');
+INSERT INTO `notif_flag` VALUES ('9', '2', '7', '1', '0');
 
 -- ----------------------------
 -- Table structure for pengguna
@@ -225,6 +324,13 @@ CREATE TABLE `pengguna` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of pengguna
+-- ----------------------------
+INSERT INTO `pengguna` VALUES ('1', 'Admin', 'admin@admin.com', null, null, null, '0', 'ACTIVE', 'admin', 'assets/dashboard/assets/images/reading.png', '202cb962ac59075b964b07152d234b70', '0', null, '0', '0');
+INSERT INTO `pengguna` VALUES ('2', 'Maha Siswa', 'mahasiswa@mahasiswa.com', '+6289987009', null, null, '0', 'ACTIVE', 'mahasiswa', 'userprofiles/Maha_Siswa_-_profil.jpg', '202cb962ac59075b964b07152d234b70', '0', null, '0', '0');
+INSERT INTO `pengguna` VALUES ('3', 'Guruururu', 'guru@guru.com', '', null, null, '0', 'ACTIVE', 'pendidik', 'userprofiles/Guruururu_-_profil.jpg', '202cb962ac59075b964b07152d234b70', '0', null, '0', '0');
+
+-- ----------------------------
 -- Table structure for permasalahan
 -- ----------------------------
 DROP TABLE IF EXISTS `permasalahan`;
@@ -247,6 +353,11 @@ CREATE TABLE `permasalahan` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of permasalahan
+-- ----------------------------
+INSERT INTO `permasalahan` VALUES ('1', 'iuaihgsvbdnma', '2018-11-06 07:18:41', '3', '1', '0', '3', '1', 'UNSOLVED', 'ACTIVE');
+
+-- ----------------------------
 -- Table structure for pesan_info
 -- ----------------------------
 DROP TABLE IF EXISTS `pesan_info`;
@@ -262,6 +373,10 @@ CREATE TABLE `pesan_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
+-- Records of pesan_info
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for riwayat_permasalahan_dilihat
 -- ----------------------------
 DROP TABLE IF EXISTS `riwayat_permasalahan_dilihat`;
@@ -272,6 +387,11 @@ CREATE TABLE `riwayat_permasalahan_dilihat` (
   `tanggal` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of riwayat_permasalahan_dilihat
+-- ----------------------------
+INSERT INTO `riwayat_permasalahan_dilihat` VALUES ('1', '2', '1', '2018-11-06 07:19:13');
 
 -- ----------------------------
 -- Table structure for tags
@@ -285,6 +405,10 @@ CREATE TABLE `tags` (
   KEY `id_materi` (`id_materi`),
   CONSTRAINT `id_materi` FOREIGN KEY (`id_materi`) REFERENCES `materi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of tags
+-- ----------------------------
 DROP TRIGGER IF EXISTS `tambah_jumlah_komen_permasalahan`;
 DELIMITER ;;
 CREATE TRIGGER `tambah_jumlah_komen_permasalahan` AFTER INSERT ON `komentar` FOR EACH ROW BEGIN
