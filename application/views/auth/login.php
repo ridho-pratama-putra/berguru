f407966b0da96f3dab883cb9dab701e4123eb39e
ridho-pratama-putra <ridhopratamavd@gmail.com>
2019-02-26 16:06:17 +0700

    2502 16:05
    - testimoniail di halaman login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -7,56 +7,35 @@
 				<img src="<?=base_url()?>assets/assets/images/thumbnails/testimoni.png" alt="" style="width: 90%;">
 			</div>
 			<div class="front-slider">
-					<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					  <!-- Indicators -->
-					  <ol class="carousel-indicators">
-					    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					    <li data-target="#myCarousel" data-slide-to="1"></li>
-					    <li data-target="#myCarousel" data-slide-to="2"></li>
-					  </ol>
+				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+					</ol>
 
-					  <!-- Wrapper for slides -->
-					  <div class="carousel-inner">
-					    <div class="item active">
-					     	<p class="quote">
-					     		“Berguru.com sangat membantu saya dalam mengembangkan penelitian studi di Sekolah Dasar, Volunteer Mahasiswa dengan sabar membantu saya dan selalu konsisten!”
-					     	</p>
-					     	<h6 class="author">
-					     		Jean Morgan
-					     	</h6>
-					     	<p class="author-pos">
-					     		Pakar Analis di Network Council
-					     	</p>
-					    </div>
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<?php foreach ($testimonial as $key => $value) { ?>
+							<div class="item <?=($key ==  0 ? 'active' : '')?>">
+								<p class="quote">
+									<?=$value->teks?>
+								</p>
+								<h6 class="author">
+									<?=$value->nama?>
+								</h6>
+								<p class="author-pos">
+									<?=$value->institusi_or_universitas?>
+								</p>
+							</div>
+						<?php } ?>
 
-					    <div class="item">
-					      	<p class="quote">
-					     		“Berguru.com sangat membantu saya dalam mengembangkan penelitian studi di Sekolah Dasar, Volunteer Mahasiswa dengan sabar membantu saya dan selalu konsisten!”
-					     	</p>
-					     	<h6 class="author">
-					     		Jean Morgan
-					     	</h6>
-					     	<p class="author-pos">
-					     		Pakar Analis di Network Council
-					     	</p>
-					    </div>
-
-					    <div class="item">
-					      	<p class="quote">
-					     		“Berguru.com sangat membantu saya dalam mengembangkan penelitian studi di Sekolah Dasar, Volunteer Mahasiswa dengan sabar membantu saya dan selalu konsisten!”
-					     	</p>
-					     	<h6 class="author">
-					     		Jean Morgan
-					     	</h6>
-					     	<p class="author-pos">
-					     		Pakar Analis di Network Council
-					     	</p>
-					    </div>
-					  </div>
-
-					  <!-- Left and right controls -->
 					</div>
+
+					<!-- Left and right controls -->
 				</div>
+			</div>
 		</div>
 		<div class="right-side-wrap">
 			<div class="row">
@@ -76,22 +55,22 @@
 						<p class="mb-30">Masuk dan mulai terhubung dengan tenaga terbaik</p>
 						<form action="<?=base_url()?>login" method="POST">
 							<div class="form-group">
-							    <label for="email">Email</label>
-							    <input type="email" class="form-control" id="email" placeholder="Masukkan Email" name="email" autofocus="">
-						  	</div>
-						  	<div class="form-group mb-10">
-							    <label for="password">Password</label>
-							    <input type="password" class="form-control" id="password" placeholder="Masukkan Password" name="password">
-						  	</div>
-						  	<div class="form-group mb-30 clearfix">
-						  		<span class="pull-left">
-						  			<input type="checkbox" name="remember"> Ingat saya
-						  		</span>
-						  		<span class="pull-right">
-						  			<a href=""><img src="<?=base_url()?>assets/assets/images/icons/question.png"" alt=""> Forgotten Password</a>
-						  		</span>
-						  	</div>
-				  			<button type="submit" class="form-control btn btn-login">Masuk ke Berguru.com</button>
+								<label for="email">Email</label>
+								<input type="email" class="form-control" id="email" placeholder="Masukkan Email" name="email" autofocus="">
+							</div>
+							<div class="form-group mb-10">
+								<label for="password">Password</label>
+								<input type="password" class="form-control" id="password" placeholder="Masukkan Password" name="password">
+							</div>
+							<div class="form-group mb-30 clearfix">
+								<span class="pull-left">
+									<input type="checkbox" name="remember"> Ingat saya
+								</span>
+								<span class="pull-right">
+									<a href=""><img src="<?=base_url()?>assets/assets/images/icons/question.png"" alt=""> Forgotten Password</a>
+								</span>
+							</div>
+							<button type="submit" class="form-control btn btn-login">Masuk ke Berguru.com</button>
 						</form>
 					</div>
 				</div>
@@ -126,21 +105,21 @@
 		</div>
 	</div>
 
-<script src="<?=base_url()?>assets/assets/libs/owl-carousel.2.3.4/owl.carousel.min.js"></script>
-<script type="text/javascript">
-	$(document).ready(function(){
-	  $(".owl-carousel").owlCarousel({
+	<script src="<?=base_url()?>assets/assets/libs/owl-carousel.2.3.4/owl.carousel.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$(".owl-carousel").owlCarousel({
 			navigation : true, // показывать кнопки next и prev 
 
-	      slideSpeed : 300,
-	      paginationSpeed : 400,
-	 
-	      items : 1, 
-	      itemsDesktop : false,
-	      itemsDesktopSmall : false,
-	      itemsTablet: false,
-	      itemsMobile : false
+			slideSpeed : 300,
+			paginationSpeed : 400,
+
+			items : 1, 
+			itemsDesktop : false,
+			itemsDesktopSmall : false,
+			itemsTablet: false,
+			itemsMobile : false
 
 		});
-	});
-</script>
+		});
+	</script>
